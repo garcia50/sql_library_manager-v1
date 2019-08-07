adaeb867b6b0b01256469199ae60acdfd19adbec
Luis Garcia <garcia50@yahoo.com>
2019-08-07 19:25:54 -0400

    Temporarily comment out unnecessary code

--- routes/books.js
+++ routes/books.js
@@ -57,24 +57,24 @@ router.post('/new', async (req, res, next) => {
   res.redirect('/books/' + book.id);
 });
 
-/* GET / retrieve book to update */
-router.get('/:id/edit', async (req, res, next) => {
-  const book = await Book.findByPk(req.params.id);
-  res.render('books/edit', { book, title: 'Edit Movie' });
-});
+// /* GET / retrieve book to update */
+// router.get('/:id/edit', async (req, res, next) => {
+//   const book = await Book.findByPk(req.params.id);
+//   res.render('books/edit', { book, title: 'Edit Movie' });
+// });
 
-/* PUT update book */
-router.put('/:id', async (req, res, next) => {
-  const book = await Book.findByPk(req.params.id);
-  await book.update(req.body);
-  res.redirect('/books/' + book.id);
-});
+// /* PUT update book */
+// router.put('/:id', async (req, res, next) => {
+//   const book = await Book.findByPk(req.params.id);
+//   await book.update(req.body);
+//   res.redirect('/books/' + book.id);
+// });
 
-/* Delete book */
-router.post('/books/:id/delete', async (req, res) => {
-  const bookToDelete = await Book.findByPk(req.params.id);
-  await bookToDelete.destroy();
-  res.redirect('/books');
-});
+// /* Delete book */
+// router.post('/books/:id/delete', async (req, res) => {
+//   const bookToDelete = await Book.findByPk(req.params.id);
+//   await bookToDelete.destroy();
+//   res.redirect('/books');
+// });
 
 module.exports = router;
